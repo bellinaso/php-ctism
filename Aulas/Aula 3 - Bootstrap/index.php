3309b9ad470e36479e098e1a23ec8df06c1ff501
bellinaso <brunobellinaso2006@gmail.com>
2024-05-29 08:59:51 -0300

    upload das aulas

---
+++ Aulas/Aula 3 - Bootstrap/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carro</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <header></header>
+    <main>
+        <div class="container-fluid">
+            <h1>Cadastro de automóveis</h1>
+            <p></p>
+        </div>
+    </main>
+</body>
+</html>
