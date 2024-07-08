a53956997e872eea7baed4f7becc3fe46183f94e
bellinaso <brunobellinaso2006@gmail.com>
2024-07-08 09:33:03 -0300

    imgs + styles change

--- Avaliação 01 - Locadora de quadras/locacao.php
+++ Avaliação 01 - Locadora de quadras/locacao.php
@@ -41,19 +41,30 @@
             background-color: #edf8f8 !important;
         }
 
-        img {
-            transition: all .35s ease-in-out !important;
+        input[type="radio"]  {
+            display: none;
         }
 
-        img:hover {
-            transform: scale(1.03);
+        input[type="radio"]:checked + label {
+            background-color: #e6ffec !important;
+            transform: scale(1.0065);
+        }
+
+        label {
+            transition: all .35s ease-in-out !important;
+            box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.1);
+        }
+
+        label:hover {
+            transform: scale(1.0065);
             box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.25);
         }
+        
     </style>
 
 </head>
 <body>
-    <header class="mb-5">
+    <header class="col-12 mb-5">
         <div class="d-flex flex-md-row flex-column justify-content-between w-100 rounded-bottom bg-dark">
             <a href="index.php" class="align-self-center">
                 <img src="imgs/arena_logo.fw-min.png" alt="logo" class="align-self-start m-3"
@@ -70,50 +81,35 @@
         </div>
     </header>
     <main class="d-flex flex-column align-items-center">
-        <div class="col-lg-8 col-md-10 col-12 d-flex flex-column justifi-content-center align-items-center">
-            <h1 class="text-center">Locação de quadras</h1>
-            <div class="col-xl-8 col-10 d-felx flex-column mb-5">
+        <h1 class="text-center">Locação de quadras</h1>
+        <div class="col-md-10 col-12 d-flex flex-lg-row flex-column justifi-content-center align-items-xl-start align-items-center">
+            <div class="col-lg-4 col-10 d-felx flex-column">
                 <h2>Quadra de Padel</h2>
-                <div class="d-flex flex-xl-row flex-column justify-content-center align-items-lg-start align-items-sm-center">
-                    <img src="imgs/quadra-de-padel.jpg" class="col-xl-6 col-10 rounded-4 m-3" style="object-fit: cover;">
-                    <p class="col-xl-6 col-10 m-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorem in iure. Deserunt fugit quas vel placeat consectetur, asperiores voluptate corrupti consequatur veniam molestias, doloremque commodi cum. Necessitatibus ipsa nihil qui incidunt laudantium inventore, nemo dolorum ad corrupti laboriosam architecto facilis placeat id cupiditate consequatur blanditiis veniam temporibus dolorem adipisci, voluptas culpa. Tenetur, optio. Sapiente qui, possimus itaque tempore vitae, facere veniam iste eveniet repellendus ex distinctio asperiores nihil eligendi nesciunt quos quo voluptas. Quia possimus rerum deserunt eos praesentium repudiandae facilis ipsam harum quae vitae consequatur libero, laboriosam aspernatur amet voluptate provident veritatis odit culpa dolorum dolor rem minima!</p>
-                </div>
-                <div class="mb-3 mt-3 d-flex justify-content-lg-end justify-content-md-start">
-                    <form action="/controller/locacaoController.php" method="POST" class="d-flex">
-                        <input type="number" name="padelHours" id="beachTenisHours" class="form-control m-2" placeholder="Quantidade de horas" min=0>
-                        <input type="submit" class="btn btn-success m-2" value="Agendar">
-                    </form>
-                </div>
+                <input type="radio" name="arena" id="padel">
+                <label for="padel" class="border bg-light rounded-5 p-3 m-3 d-flex flex-column justify-content-center align-items-lg-start align-items-sm-center">
+                    <img src="imgs/quadra-de-padel.jpg" class="col-12 rounded-4" style="object-fit: cover;">
+                    <p class="col-12 pt-3">Descubra a paixão pelo padel no Saúde Beach Arena!
+                    Nossas quadras modernas oferecem o ambiente perfeito para aprimorar suas habilidades e se divertir. Seja você um iniciante ou um jogador experiente, nossa comunidade acolhedora e eventos emocionantes fazem do nosso clube o destino ideal para todos. Venha jogar e faça parte dessa energia vibrante!</p>
+                </label>
             </div>
-
-            <div class="col-xl-8 col-10 d-felx flex-column mb-5">
+            <div class="col-lg-4 col-10 d-felx flex-column">
+                <h2>Quadra de Beach Tennis</h2>
+                <input type="radio" name="arena" id="beach-tennis">
+                <label for="beach-tennis" class="border bg-light rounded-5 p-3 m-3 d-flex flex-column justify-content-center align-items-lg-start align-items-sm-center">
+                    <img src="imgs/quadra-de-beach-tennis2.jpg" class="col-12 rounded-4" style="object-fit: cover;">
+                    <p class="col-12 pt-3">Experimente a emoção do beach tennis no Saúde Beach Arena!
+                    Nossas quadras de areia são ideais para partidas emocionantes e momentos de lazer. Jogue com amigos, participe de torneios ou melhore seu jogo com nossas aulas. No Saúde Beach Arena, promovemos um estilo de vida ativo e saudável através do esporte. Venha e sinta a vibração da praia em cada jogo!</p>
+                </label>
+            </div>
+            <div class="col-lg-4 col-10 d-felx flex-column">
                 <h2>Quadra de Pickleball</h2>
-                <div class="d-flex flex-xl-row flex-column justify-content-center align-items-lg-start align-items-sm-center">
-                    <p class="col-xl-6 col-10 m-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorem in iure. Deserunt fugit quas vel placeat consectetur, asperiores voluptate corrupti consequatur veniam molestias, doloremque commodi cum. Necessitatibus ipsa nihil qui incidunt laudantium inventore, nemo dolorum ad corrupti laboriosam architecto facilis placeat id cupiditate consequatur blanditiis veniam temporibus dolorem adipisci, voluptas culpa. Tenetur, optio. Sapiente qui, possimus itaque tempore vitae, facere veniam iste eveniet repellendus ex distinctio asperiores nihil eligendi nesciunt quos quo voluptas. Quia possimus rerum deserunt eos praesentium repudiandae facilis ipsam harum quae vitae consequatur libero, laboriosam aspernatur amet voluptate provident veritatis odit culpa dolorum dolor rem minima!</p>
-                    <img src="imgs/quadra-de-pickleball.jpg" class="col-xl-6 col-10 rounded-4 m-3" style="object-fit: cover;">
-                </div>
-                <div class="mb-3 mt-3 d-flex justify-content-start">
-                    <form action="/controller/locacaoController.php" method="POST" class="d-flex">
-                        <input type="number" name="pickleballHours" id="beachTenisHours" class="form-control m-2" placeholder="Quantidade de horas" min=0>
-                        <input type="submit" class="btn btn-success m-2" value="Agendar">
-                    </form>
-                </div>
+                <input type="radio" name="arena" id="piclkeball">
+                <label for="piclkeball" class="border bg-light rounded-5 p-3 m-3 d-flex flex-column justify-content-center align-items-lg-start align-items-sm-center">
+                    <img src="imgs/quadra-de-pickleball.jpg" class="col-12 rounded-4" style="object-fit: cover;">
+                    <p class="col-12 pt-3">Divirta-se jogando pickleball no Saúde Beach Arena!
+                    Nossas quadras estão prontas para receber jogadores de todas as idades e níveis. O pickleball é um esporte dinâmico e acessível que combina diversão e competição. Junte-se a nós, participe dos nossos eventos e torneios, e descubra por que o pickleball está conquistando corações. Aqui, a diversão é garantida!</p>
+                </label>
             </div>
-
-            <div class="col-xl-8 col-10 d-felx flex-column mb-5">
-                <h2>Quadra de Beach Tenis</h2>
-                <div class="d-flex flex-xl-row flex-column justify-content-center align-items-lg-start align-items-sm-center">
-                    <img src="imgs/quadra-de-beach-tennis.jpg" class="col-xl-6 col-10 rounded-4 m-3" style="object-fit: cover;">
-                    <p class="col-xl-6 col-10 m-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorem in iure. Deserunt fugit quas vel placeat consectetur, asperiores voluptate corrupti consequatur veniam molestias, doloremque commodi cum. Necessitatibus ipsa nihil qui incidunt laudantium inventore, nemo dolorum ad corrupti laboriosam architecto facilis placeat id cupiditate consequatur blanditiis veniam temporibus dolorem adipisci, voluptas culpa. Tenetur, optio. Sapiente qui, possimus itaque tempore vitae, facere veniam iste eveniet repellendus ex distinctio asperiores nihil eligendi nesciunt quos quo voluptas. Quia possimus rerum deserunt eos praesentium repudiandae facilis ipsam harum quae vitae consequatur libero, laboriosam aspernatur amet voluptate provident veritatis odit culpa dolorum dolor rem minima!</p>
-                </div>
-                <div class="mb-3 mt-3 d-flex justify-content-lg-end justify-content-md-start">
-                    <form action="/controller/locacaoController.php" method="POST" class="d-flex">
-                        <input type="number" name="beachTenisHours" id="beachTenisHours" class="form-control m-2" placeholder="Quantidade de horas" min=0>
-                        <input type="submit" class="btn btn-success m-2" value="Agendar">
-                    </form>
-                </div>
-            </div>
-
         </div>
     </main>
 </body>
